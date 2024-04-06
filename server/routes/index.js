ace089a60d8c65f326658841a6d4f36e069a3822
Anunay Kumar <anunaylife@gmail.com>
2024-04-07 00:41:11 +0530

    cors config

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var cors=require('cors')
+
+app.use(cors(
+  {
+    origin:["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials:true
+  }
+))
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
